f201498ff4b852976cbc4aecb45c12c14b13f278
mdhabibur-rahman <145204698+mdhabibur-rahman@users.noreply.github.com>
2024-10-24 12:56:33 +0600

    add javascript file

---
+++ main.js
@@ -0,0 +1,31 @@
+// let num = prompt("Enter lenght of number: ")
+
+// for(let i = num ; i>=0 ; i--){
+//     console.log(i);
+// }
+
+// for(let i = 0 ; i<=num ; i++){
+//     console.log(i);
+// }
+
+// console.log("hello");
+
+
+
+// let inputValue= prompt("Enter the number: ");
+// let isprime=inputValue==1? false:true;  //bcoz 1 is not prime
+
+// for(let i=2;i<inputValue;i++){
+//   inputValue%i==0? isprime*=false :isprime*=true;
+// };
+
+// alert(`${inputValue} is ${isprime? 'prime':'not prime'} number`);
+
+var day = "riday";
+
+var expectedGuests = 100 * ((day==='Friday')*1.5 || 1);
+console.log(expectedGuests);
+
+var multiplier = day === 'Friday' ? 1.5 : 1;
+var expectedGuests = 100 * multiplier;
+console.log(expectedGuests);
